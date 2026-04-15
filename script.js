6b5a01c329037ee2be1a4518f19f51b08d278b41
XiomaraJimenezz <est.nicoll.jimenez@unimilitar.edu.co>
2026-04-14 20:57:38 -0500

    se inicializa canvas y funcion dibujar

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    console.log("js conectado");
+}
+window.onload = function() {
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    function dibujar() {
+        console.log("canvas listo");
+    }
+
+}
